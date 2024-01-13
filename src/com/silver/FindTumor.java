aa36078f6ed41e75576610d1060ab1e6cee51399
Patrick Hanes <phanes72@gmail.com>
2024-01-13 09:06:10 -0700

    Side by Side is close

--- src/com/silver/FindTumor.java
+++ src/com/silver/FindTumor.java
@@ -1,61 +1,63 @@
 package com.silver;
 
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class FindTumor {
+	private static String content;
 	private static Set<String> listOfLetters = new HashSet<String>();
+	private static ArrayList<String> groupList = new ArrayList<String>();
 
-    public static void main(String[] args) {
-        args = new String[1];
-    	
-        //Get file
-        args[0] = "test.in";
-    	
-    	if (args.length != 1) {
-            System.err.println("Usage: java FindTumor <file-path>");
-            System.exit(1);
-        }
+	public static void main(String[] args) {
+		args = new String[1];
 
-        String filePath = args[0];
-        try {
-            String content = readContent(filePath);
-            boolean hasMultipleGroups = hasMultipleGroups(content);
+		// Get file
+		args[0] = "resources/test.in";
 
-            if (hasMultipleGroups) {
-                System.out.println("True");
-            } else {
-                System.out.println("False");
-            }
+		if (args.length != 1) {
+			System.err.println("Usage: java FindTumor <file-path>");
+			System.exit(1);
+		}
 
-            int[] dimensions = calculateDimensions(content);
-            System.out.println("Dimensions: " + dimensions[0] + " x " + dimensions[1]);
+		String filePath = args[0];
+		try {
+			content = readContent(filePath);
+			boolean hasMultipleGroups = hasMultipleGroups(content);
 
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-        }
-    }
+			if (hasMultipleGroups) {
+				System.out.println("True");
+			} else {
+				System.out.println("False");
+			}
 
-    private static String readContent(String filePath) throws IOException {
-        Path path = Paths.get(filePath);
-        return Files.readString(path);
-    }
+			int[] dimensions = calculateDimensions(content);
+			System.out.println("Dimensions: " + dimensions[0] + " x " + dimensions[1]);
 
-    private static boolean hasMultipleGroups(String content) {
-        String[] rows = content.split("\n");
-        Set<Character> currentGroup = new HashSet<>();
-        
-        populateLetterArray(content);
-        
-        searchForLetterGroups();
+		} catch (IOException e) {
+			System.err.println("Error reading file: " + e.getMessage());
+		}
+	}
 
-        //Loop through the rows
+	protected static String readContent(String filePath) throws IOException {
+		Path path = Paths.get(filePath);
+		return Files.readString(path);
+	}
+
+	protected static boolean hasMultipleGroups(String content) {
+		//String[] rows = content.split("\n");
+		Set<Character> currentGroup = new HashSet<>();
+
+		populateUniqueLetterArray(content);
+
+		searchForLetterGroups();
+
+		// Loop through the rows
 //        for (int i = 0; i < rows.length; i++) {
 //            
 //        	//Loop through the columns
@@ -76,44 +78,59 @@ public class FindTumor {
 //            }
 //        }
 
-        return currentGroup.size() > 1;
-    }
+		return currentGroup.size() > 1;
+	}
 
-    private static int[] calculateDimensions(String content) {
-        String[] rows = content.split("\n");
-        int length = rows.length;
-        int width = rows.length > 0 ? rows[0].length() : 0;
-        return new int[]{length, width};
-    }
-    
-    //find all the unique letters and create an Set array
-    private static void populateLetterArray(String content) {
-    	
-    	String[] rows = content.split("\n");
-    	
-    	for (int i = 0; i < rows.length; i++) {
-    		
-    		for (int j = 0; j < rows[i].length(); j++) {
-    			
-    			String currentLetter = "" + rows[i].charAt(j);
-    			if(!currentLetter.isBlank()) {    				
-    				listOfLetters.add(currentLetter);
-    			}
-    			
-    		}
-    	}
-    }
-    
-    //search for each letter structure of side by side to establish a single block
-    private static void searchForLetterGroups() {
-    	if(listOfLetters.contains("x")) {
-    		System.out.println("A single block has been identified");
-    	} else {
-    		System.out.println("A single block was not found");
-    	}
-    }
-    
-    //when a new letter is found in the row drop down and see if that letter is found
-    
-    //
+	protected static int[] calculateDimensions(String content) {
+		String[] rows = content.split("\n");
+		int length = rows.length;
+		int width = rows.length > 0 ? rows[0].length() : 0;
+		return new int[] { length, width };
+	}
+
+	// Populate a static content variable to carry the matrix data
+
+	// find all the unique letters in the matrix
+	protected static void populateUniqueLetterArray(String content) {
+
+		String[] rows = content.split("\n");
+
+		for (int i = 0; i < rows.length; i++) {
+
+			for (int j = 0; j < rows[i].length(); j++) {
+
+				String currentLetter = "" + rows[i].charAt(j);
+				if (!currentLetter.isBlank()) {
+					listOfLetters.add(currentLetter);
+				}
+
+			}
+		}
+	}
+
+	// search for each letter structure of a side-by-side occurrence to establish a
+	// single block
+	protected static void searchForLetterGroups() {
+		Iterator<String> listOfUniqueLetterIterator = listOfLetters.iterator();
+
+		// Loop thru the Unique List of letters
+		while (listOfUniqueLetterIterator.hasNext()) {
+			String uniqueLetter = listOfUniqueLetterIterator.next();
+			String uniqueLetterSideBySide = uniqueLetter + uniqueLetter;
+
+			
+			if (content.contains(uniqueLetterSideBySide)) {
+				System.out.println("Side by Side found for " + uniqueLetter);
+				String groupFoundString = "Side by Side found for " + uniqueLetter;
+				groupList.add(groupFoundString);
+			} 
+
+		}
+
+	}
+
+	// when a new letter is found in the row drop down and see if that letter is
+	// found
+
+	//
 }
